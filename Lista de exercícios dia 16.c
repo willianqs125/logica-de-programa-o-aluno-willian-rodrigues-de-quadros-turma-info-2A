8f2d3a8f346aff46e4a7b48a59c9ea77caf51d95
willianqs125 <willianrodriguesdequadros@gmail.com>
2025-07-23 10:09:44 -0300

    Update Lista de exercícios dia 16.c

--- Lista de exercícios dia 16.c
+++ Lista de exercícios dia 16.c
@@ -16,3 +16,30 @@ int main()
         printf(" numeros não são multiplos.");
     }
 }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int primeiron, segundon, soma, subt, mult, divi, resto;
+
+    printf("Informe um numero: ");
+    scanf("%d", &primeiron);
+    printf("Informe o segundo numero: ");
+    scanf("%d", &segundon);
+
+    soma = primeiron + segundon;
+    sub = primeiron - segundon;
+    mult = primeiron * segundon;
+    div = primeiron/segundon;
+    resto = primeiron%segundon;
+
+    printf("Soma: %d + %d = %d\n", primeiron, segundon, soma);
+    printf("Subtração: %d - %d = %d\n", primeiron, segundon, subt);
+    printf("Multiplicação: %d * %d = %d\n", primeiron, segundon, mult);
+    printf("Divisão: %d / %d = %d\n", primeiron, segundon, divi);
+    printf("Resto: %d %% %d = %d\n", primeiron, segundon, resto);
+
+}
